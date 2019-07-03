14366a9908a943b88b2b759af734cadc9b32c331
KangYein <yeinkang04@gmail.com>
2019-07-03 17:06:45 +0900

    7월1일

---
+++ 04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+ 	float a=10,b=20;
+ 	double percent = a/100;
+ 	double c= 0.3;
+ 	printf("percent is %f. \n",percent);
+ 	printf("percent is %e. \n",percent);
+ 	printf("percent is %a. \n",percent);
+ 	printf("a:%f,b:%e",a,b);
+ }
