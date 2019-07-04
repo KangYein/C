b49573249ef35bbab86d728b19c445b7d7590914
KangYein <yeinkang04@gmail.com>
2019-07-04 17:21:10 +0900

    7월 4일

---
+++ 4_03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	int i,j,n;
+	printf("±¸±¸´Ü:\n");
+	//scanf("%d",&n);
+	for(i=1;i<=9;i++){
+		printf("\n");
+		for(j=1;j<=9; j++){
+		printf("%d * %d = %d \t",i,j,i*j);
+	}
+	}
+}
