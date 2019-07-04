d21544f2a43f36f98cf0ca7cbad971578bddd4d3
KangYein <yeinkang04@gmail.com>
2019-07-04 17:22:04 +0900

    7월 4일

---
+++ 4_04.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+	int a;
+	a=output(3,5);
+	printf("%d \n",a);
+}
+
+int output(int b1, int b2){//돌려받을 때 
+	int b3;
+	b3=b1+b2;
+	printf("AAAA \n");
+	return b3;
+} 
