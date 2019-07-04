4787e80f83f15de8be458d8406a54cdffc275553
KangYein <yeinkang04@gmail.com>
2019-07-04 16:23:27 +0900

    7월1일

---
+++ 07.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){
+ 	int num1,num2;
+ 	
+ 	printf("Enter the 1st number:");
+ 	scanf("%d",&num1);
+ 	printf("Enter the 2nd number:");
+ 	scanf("%d",&num2);
+ 	printf("summary: %d \n",num1+num2);
+ 	
+ 	return 0;
+ }
