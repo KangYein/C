58ec1d3d5af43e49c9d978803623f27ce58954ac
KangYein <yeinkang04@gmail.com>
2019-07-04 17:14:56 +0900

    7월 3일

---
+++ 3_02.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+	int a,b,answer,t=1;
+	answer=a+b;
+	printf("두 수 입력:\n");
+	scanf("%d %d",&a,&b);
+	//printf("%d+%d=%d \n",a,b,a+b);
+	
+	re_answer:
+	printf("두 수의 합:");
+	scanf("%d",&answer);
+	if(answer==(a+b)){
+		printf("정답!!\n");
+	}
+	else{
+		printf("오답!!\n");	
+		t=t+1;//오답 횟수 구하기 
+		goto re_answer;
+	}
+	printf("정답 확률:%.2f\n",1.0/t);
+}
